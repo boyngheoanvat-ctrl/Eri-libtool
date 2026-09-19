d354aa2bda3d7d6a1662e383263e4e7256b6a732
boyngheoanvat-ctrl <boyngheoanvat@gmail.com>
2026-09-19 11:48:45 +0700

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,212 @@
+#include <jni.h>     
+#include <pthread.h> 
+#include <thread>
+#include <unistd.h>              
+#include "Il2cpp/Il2cpp.h"       
+#include "Il2cpp/il2cpp-class.h" 
+#include "Includes/Logger.h"     
+#include "Includes/Utils.h"      
+#include "Includes/obfuscate.h"  
+#include "Menu/ImGui.h"
+#include "Tool/Keyboard.h"
+#include "Tool/Tool.h"
+#include "Tool/Util.h"
+#include "imgui/imgui.h"
+#include "imgui/imgui_internal.h"
+#include "sstream"
+#include <android/native_window_jni.h>
+#include "Tool/Unity.h"
+
+void logcatJson(nlohmann::ordered_json &json)
+{
+    auto str = json.dump(4, '#');
+    std::istringstream iss(str);
+    std::string line;
+    while (std::getline(iss, line))
+    {
+        usleep(100);
+        LOGD("%s", line.c_str());
+    }
+}
+
+template <typename T>
+void ConfigSet(const char *key, T value);
+
+Il2CppImage *g_Image = nullptr;
+std::vector<MethodInfo *> g_Methods;
+extern std::unordered_map<void *, HookerData> hookerMap;
+extern int maxLine;
+extern ImVec2 initialScreenSize;
+
+bool collapsed = false;
+bool fullScreen = false;
+bool resetWindow = false;
+int selectedScale = 3;
+int selectedTheme = 0;
+
+bool doChangeScale = false;
+bool doChangeTheme = false;
+
+constexpr std::array<const char *, 7> possibleScale = {
+    "Smallest", "Smaller", "Small", "Default", "Large", "Larger", "Largest",
+};
+constexpr std::array<float, 7> scaleFactors = {0.25f, 0.5f, 0.75f, 1.0f, 1.25f, 1.5f, 2.0f};
+
+constexpr std::array<const char *, 3> possibleThemes = {
+    "Dark", "Light", "Classic",
+};
+
+ImGuiStyle initialStyle;
+const char *title = OBFUSCATE("IL2cpp Tool By Your Name");
+
+void draw_thread()
+{
+    static ImVec2 lastSize = ImVec2(0, 0);
+    static ImVec2 lastPos = ImVec2(0, 0);
+
+    if (resetWindow)
+    {
+        resetWindow = false;
+        if (fullScreen)
+        {
+            ImGui::SetNextWindowPos(ImVec2(0, 0));
+            auto screenSize = ImGui::GetIO().DisplaySize;
+            ImGui::SetNextWindowSize(screenSize);
+        }
+        else
+        {
+            ImGui::SetNextWindowPos(lastPos);
+            ImGui::SetNextWindowSize(lastSize);
+        }
+    }
+    if (fullScreen)
+    {
+        ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(0, ImGui::GetFrameHeight()));
+    }
+    int i = 0;
+    auto drawList = ImGui::GetBackgroundDrawList();
+
+    for (auto &v : HookerData::visited)
+    {
+        if (v.name.empty())
+            continue;
+        char label[256]{0};
+        sprintf(label, "%s", v.name.c_str());
+        if (v.hitCount > 0)
+        {
+            sprintf(label, "%s (%dx)", label, v.hitCount);
+        }
+        auto labelSize = ImGui::CalcTextSize(label);
+        ImVec2 labellPos{20, 150 + (labelSize.y * i)};
+
+        auto dt = ImGui::GetIO().DeltaTime;
+        constexpr ImVec4 GREEN = {0.f, 1.f, 0.f, 1.f};
+        ImColor color = ImColor(1.f, 1.f, 1.f, 1.f);
+        if (v.time > 0.f)
+        {
+            v.time -= dt;
+            auto t = v.time;
+            color = ImColor(ImLerp(color.Value.x, GREEN.x, t), ImLerp(color.Value.y, GREEN.y, t),
+                            ImLerp(color.Value.z, GREEN.z, t), 1.f);
+        }
+        v.goneTime -= dt;
+        if (v.goneTime > 0.f && v.goneTime <= 1.f)
+        {
+            auto t = v.goneTime;
+            color.Value.w = ImLerp(0.f, color.Value.w, t);
+        }
+        if (v.goneTime <= 0.f)
+        {
+            v.name = "";
+        }
+
+        drawList->AddRectFilled(labellPos, {labellPos.x + labelSize.x, labellPos.y + labelSize.y},
+                                IM_COL32(0, 0, 0, 100));
+        drawList->AddText(labellPos, color, label);
+        i++;
+    }
+    collapsed = !ImGui::Begin(title, nullptr, (fullScreen ? ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove : 0));
+    if (fullScreen)
+    {
+        ImGui::PopStyleVar();
+    }
+
+    Keyboard::Update();
+    static bool changeToToolsTab = false;
+    if (ImGui::BeginTabBar("mainTabber"))
+    {
+        if (ImGui::BeginTabItem("Tools", nullptr, changeToToolsTab ? ImGuiTabItemFlags_SetSelected : 0))
+        {
+            changeToToolsTab = false;
+            if (ImGui::Checkbox("Fullscreen", &fullScreen))
+            {
+                if (fullScreen)
+                {
+                    lastSize = ImGui::GetWindowSize();
+                    lastPos = ImGui::GetWindowPos();
+                }
+                resetWindow = true;
+            }
+            Tool::Draw();
+            ImGui::EndTabItem();
+        }
+
+        if (!hookerMap.empty())
+        {
+            if (ImGui::BeginTabItem("Tracer"))
+            {
+                ImGui::Text("Traced method count : %zu", hookerMap.size());
+                std::vector<HookerData *> sortedHooker;
+                for (auto &[name, data] : hookerMap)
+                {
+                    if (data.hitCount > 0)
+                        sortedHooker.push_back(&data);
+                }
+                if (!sortedHooker.empty())
+                {
+                    if (ImGui::Button("Quick Restore"))
+                    {
+                        ImGui::OpenPopup("QuickRestorePopup");
+                    }
+                    std::sort(sortedHooker.begin(), sortedHooker.end(),
+                              [](const HookerData *a, const HookerData *b) { return a->hitCount > b->hitCount; });
+                    ImGui::BeginChild("TracerList", ImVec2(0, 0), ImGuiChildFlags_None,
+                                      ImGuiWindowFlags_HorizontalScrollbar);
+                    auto &tab = Tool::GetFirstTab();
+                    for (auto v : sortedHooker)
+                    {
+                        ImGui::PushID(v->method);
+                        bool opened =
+                            tab.MethodViewer(v->method->getClass(), v->method, tab.getCachedParams(v->method));
+                        if (!opened && !changeToToolsTab && ImGui::IsItemHeld())
+                        {
+                            changeToToolsTab = true;
+                            Tool::OpenNewTabFromClass(v->method->getClass()).setOpenedTab = true;
+                            ImGui::PopID();
+                            break;
+                        }
+                        ImGui::PopID();
+                    }
+                    ImGui::EndChild();
+                }
+                ImGui::EndTabItem();
+            }
+        }
+        ImGui::EndTabBar();
+    }
+    ImGui::End();
+}
+
+void *hack_thread(void *)
+{
+    logger::Clear();
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    initModMenu((void *)draw_thread, nullptr);
+    return nullptr;
+}
+
+__attribute__((constructor)) void lib_main()
+{
+    pthread_t ptid;
+    pthread_create(&ptid, nullptr, hack_thread, nullptr);
+}
